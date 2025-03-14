25cfea38d1e6c1bd172ebeea98d6c71c1dfdb162
koofte <91737069+kootfe@users.noreply.github.com>
2025-03-14 21:32:04 +0300

    Update main.ts, README.md

--- main.ts
+++ main.ts
@@ -3,33 +3,74 @@ let temp = 0;
 let mode = 0;
 let maxModes = 3;
 
+let logos: Image[] = [
+    images.createImage(`
+    . # # # .
+    # . # . #
+    # # # # #
+    # . # . #
+    . # # # .`),
+    images.createImage(`
+    . # # # .
+    # . . . #
+    . . # . .
+    . # . # .
+    . . # . .`),
+    images.createImage(`
+    . . . . .
+    . . . # #
+    . # # . #
+    . . # # .
+    # # # # #`)
+];
+
 radio.setGroup(radioGroup);
 radio.sendNumber(0);
 
-input.onButtonPressed(Button.A, function() {
+input.onButtonPressed(Button.A, function () {
     switch (mode) {
-        case 0: 
-            temp = temp < maxModes ? temp+1 : 0;
-            basic.showNumber(temp);
+        case 0:
+            mode = 3;
+            menuDep();
+            break;
+        case 3:
+            menuDep();
             break;
         default:
             break;
     }
-})
+});
 
 input.onButtonPressed(Button.AB, function () {
     switch (mode) {
-        case 0:
+        case 3:
             mode = temp;
-            basic.showNumber(mode)
-            basic.pause(100)
-            basic.clearScreen()
-            basic.showNumber(mode)
-            basic.pause(100)
-            basic.clearScreen()
-            basic.showNumber(mode)
+            basic.showNumber(mode);
+            basic.showIcon(IconNames.Yes);
+            basic.showNumber(mode);
+            basic.clearScreen();
+            mode = 0;
             break;
         default:
             break;
     }
-})
+});
+
+input.onButtonPressed(Button.B, function () {
+    switch (mode) {
+        case 0:
+            basic.showNumber(radioGroup);
+            break;
+        default:
+            break;
+    }
+});
+
+function menuDep() {
+    temp = temp < maxModes - 1 ? temp + 1 : 0;
+    if (temp < logos.length) {
+        logos[temp].showImage(0);
+    } else {
+        basic.showNumber(temp);
+    }
+}
