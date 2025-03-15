fb48620baa8eeff344582b1247aadccd49e4e67a
koofte <91737069+kootfe@users.noreply.github.com>
2025-03-15 15:04:33 +0300

    Kft_Get-Give 1.2.0 - Directions & Gestures Update

--- main.ts
+++ main.ts
@@ -1,71 +1,27 @@
-let radioGroup = 69;
-let temp = 0;
-let mode = 0;
-let maxModes = 3;
-
-let logos: Image[] = [
-    images.createImage(`
-    . # # # .
-    # . # . #
-    # # # # #
-    # . # . #
-    . # # # .`),
-    images.createImage(`
-    . # # # .
-    # . . . #
-    . . # . .
-    . # . # .
-    . . # . .`),
-    images.createImage(`
-    . . . . .
-    . . . # #
-    . # # . #
-    . . # # .
-    # # # # #`)
-];
-
-radio.setGroup(radioGroup);
-radio.sendNumber(0);
-
-input.onButtonPressed(Button.A, function () {
-    switch (mode) {
-        case 0:
-            mode = 3;
-            menuDep();
-            break;
-        case 3:
-            menuDep();
-            break;
-        default:
-            break;
+input.onGesture(Gesture.LogoDown, function () {
+    if (drive) {
+        radio.sendNumber(1);
     }
-});
+})
 
-input.onButtonPressed(Button.AB, function () {
-    switch (mode) {
-        case 3:
-            mode = temp;
-            basic.showNumber(mode);
-            basic.showIcon(IconNames.Yes);
-            basic.showNumber(mode);
-            basic.clearScreen();
-            mode = 0;
-            break;
-        default:
-            break;
+input.onGesture(Gesture.LogoUp, function () {
+    if (drive) {
+        radio.sendNumber(2);
     }
-});
-
-input.onButtonPressed(Button.B, function () {
-    switch (mode) {
-        case 0:
-            basic.showNumber(radioGroup);
-            break;
-        default:
-            break;
+})
+ 
+input.onGesture(Gesture.TiltLeft, function () {
+    if (drive) {
+        radio.sendNumber(3);
     }
-});
-
+})
+ 
+input.onGesture(Gesture.TiltRight, function () {
+    if (drive) {
+        radio.sendNumber(4);
+    }
+})
+ 
 function menuDep() {
     temp = temp < maxModes - 1 ? temp + 1 : 0;
     if (temp < logos.length) {
@@ -74,3 +30,69 @@ function menuDep() {
         basic.showNumber(temp);
     }
 }
+
+input.onButtonPressed(Button.A, function () {
+    if (mode == 0) {
+        mode = 3;
+        drive = false;
+        menuDep();
+    } else if (mode == 1) {
+        temp = temp + 1 < 255 ? temp + 1 : 0;
+        basic.showNumber(temp);
+    } else if (mode == 3) {
+        menuDep();
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (mode == 0) {
+        radio.sendNumber(0);
+    } else if (mode == 1) {
+        radioGroup = temp;
+        radio.setGroup(radioGroup);
+        mode = 0
+    } else if (mode == 3) {
+        mode = temp;
+        basic.showNumber(mode);
+        basic.showIcon(IconNames.Yes);
+        basic.showNumber(mode);
+        basic.clearScreen();
+        mode = temp;
+        drive = temp == 0 ? true : false;
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (mode == 0) {
+        basic.showNumber(mode)
+    } else if (mode == 1) {
+        temp = temp - 1 > 0 ? temp - 1 : 255;
+        basic.showNumber(temp);
+    }
+})
+let mode = 0
+let logos: Image[] = []
+let radioGroup = 0
+let temp = 0
+let drive = true;
+radioGroup = 69
+let maxModes = 3
+logos = [images.createImage(`
+    . # # # .
+    # . # . #
+    # # # # #
+    # . # . #
+    . # # # .
+    `), images.createImage(`
+    . # # # .
+    # . . . #
+    . . # . .
+    . # . # .
+    . . # . .
+    `), images.createImage(`
+    . . . . .
+    . . . # #
+    . # # . #
+    . . # # .
+    # # # # #
+    `)]
+radio.setGroup(radioGroup)
+radio.sendNumber(0)
